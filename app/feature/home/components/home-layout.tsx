de6d7015bb85cc2394299eabd1c34549d32bd42e
ethang <hello@ethang.email>
2022-07-19 12:51:42 -0500

    override homepage

--- app/feature/home/components/home-layout.tsx
+++ app/feature/home/components/home-layout.tsx
@@ -21,7 +21,36 @@ export const HomeLayout = (): JSX.Element | null => {
           src={homeContent.image.url}
         />
       </div>
-      <p>{homeContent.content}</p>
+      <div style={{ maxWidth: '600px' }}>
+        <p>
+          A small community that has 202 homes for full time and part time
+          residents. Located in Benton County Missouri!
+        </p>
+        <p>
+          The community has a pool, pavilion (which can be rented by a resident
+          by contacting a trustee) horseshoe pits, and basketball hoops.
+        </p>
+        <p>
+          Close by (a golf cart ride away) is Sterett Creek Marina and
+          Campground.
+        </p>
+        <p>
+          <strong>Contacts</strong>
+        </p>
+        <p>Trustees</p>
+        <ul>
+          <li>Debra Anderson 660-233-3673</li>
+          <li>Rita StJohn 417-268-5539</li>
+          <li>Mike Robey 573-326-0217</li>
+          <li>Lowell Washer 816-830-3342</li>
+          <li>Caressa Maher 913-603-2286</li>
+        </ul>
+        <p>Water Technicians</p>
+        <ul>
+          <li>Kirk Leimkuehler, Chief Water Operator, 660-223-2310</li>
+          <li>Mike Blyth, Back-up Water Operator, 816-316-9610</li>
+        </ul>
+      </div>
     </Container>
   );
 };
